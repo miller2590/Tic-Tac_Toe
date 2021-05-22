e9aa78833f47c3b468ce7a3fe2c0e630e2dc3b4c
miller2590 <59415200+miller2590@users.noreply.github.com>
2021-05-22 15:29:01 -0500

    created is winner, and full board

--- app_functions.cpp
+++ app_functions.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 #include <array>
 
-char grid[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+std::string grid[9] = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
 int player = 1;
 int position = 0;
 
 void welcome() {
+
   std::cout << "===========\n";
   std::cout << "Tic Tac Toe\n";
   std::cout << "===========\n\n";
@@ -15,19 +16,69 @@ void welcome() {
   std::cout << "This is how you will choose your positions!\n\n";
 
   std::cout << "     |     |      \n";
-  std::cout << "  0  |  1  |  2   \n";
+  std::cout << "  1  |  2  |  3   \n";
   std::cout << "_____|_____|_____ \n";
   std::cout << "     |     |      \n";
-  std::cout << "  3  |  4  |  5   \n";
+  std::cout << "  4  |  5  |  6   \n";
   std::cout << "_____|_____|_____ \n";
   std::cout << "     |     |      \n";
-  std::cout << "  6  |  7  |  8   \n";
+  std::cout << "  7  |  8  |  9   \n";
   std::cout << "     |     |      \n\n";
   
 }
 
+bool is_winner() {
 
-void create_board() {
+  bool winner = false;
+  
+  // rows
+  if ((grid[0] == grid[1]) && (grid[1] == grid[2]) && grid[0] != " ") {
+    winner = true;
+  } else if ((grid[3] == grid[4]) && (grid[3] == grid[5]) && grid[3] != " ") {
+    winner = true;
+  } else if ((grid[6] == grid[7]) && (grid[6] == grid[8]) && grid[6] != " ") {
+    winner = true;
+  }
+
+  // columns
+  else if ((grid[0] == grid[3]) && (grid[0] == grid[6]) && grid[0] != " ") {
+    winner = true;
+  } else if ((grid[1] == grid[4]) && (grid[1] == grid[7]) && grid[1] != " ") {
+    winner = true;
+  } else if ((grid[2] == grid[5]) && (grid[2] == grid[8]) && grid[2] != " ") {
+    winner = true;
+  } 
+  
+  // diagonals
+  else if ((grid[0] == grid[4]) && (grid[0] == grid[8]) && grid[0] != " ") {
+    winner = true;
+  }
+  else if ((grid[2] == grid[4]) && (grid[2] == grid[6]) && grid[2] != " ") {
+    winner = true;
+  }
+
+  return winner;
+
+}
+
+bool full_board() {
+
+  bool full = true;
+
+  for (int i = 0; i < 9; i++) {
+
+    if (grid[1] == " ") {
+      full = false;
+      
+    }
+    
+  }
+
+  return full;
+  
+}
+
+void create_grid() {
 
   std::cout << "     |     |     \n";
 
@@ -46,3 +97,4 @@ void create_board() {
 
   std::cout << "     |     |     \n\n";
 }
+
